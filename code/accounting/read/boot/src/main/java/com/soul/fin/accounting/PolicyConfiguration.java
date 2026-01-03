e35db1b65782eb52b1742084a345656645027cec
Guilherme R <guibonnis@gmail.com>
2026-01-03 01:57:03 +0000

    Add read modules

---
+++ code/accounting/read/boot/src/main/java/com/soul/fin/accounting/PolicyConfiguration.java
@@ -0,0 +1,10 @@
+package com.soul.fin.accounting;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PolicyConfiguration {
+
+
+}
+
---
+++ code/accounting/read/boot/src/main/java/com/soul/fin/accounting/PolicyServicesConfiguration.java
@@ -0,0 +1,9 @@
+package com.soul.fin.accounting;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PolicyServicesConfiguration {
+
+}
+
